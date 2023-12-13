de8fac3cc07a025b328cb9e4f7c29754d4de2e4a
cracker <crackerwork@126.com>
2023-12-13 17:21:31 +0800

    初始化

---
+++ Custom.Command.Line/CommandBase.cs
@@ -0,0 +1,11 @@
+﻿namespace Custom.Command.Line
+{
+    internal abstract class CommandBase
+    {
+        public abstract (string name, string desc) ID { get; }
+
+        protected virtual List<CommandOption> Options { get; }
+
+        public abstract Task ExecuteAsync(string[] args);
+    }
+}
---
+++ Custom.Command.Line/Commands/GenFormDataCommand.cs
@@ -0,0 +1,30 @@
+﻿namespace Custom.Command.Line.Commands
+{
+    internal class GenFormDataCommand : CommandBase
+    {
+        public override (string name, string desc) ID => ("gen", "生成常用测试数据");
+
+        protected override List<CommandOption> Options => new()
+        {
+                new() {
+                    Name = "--phone",
+                    Action = () =>
+                    {
+                        Console.WriteLine("125467865321");
+                    }
+                }
+        };
+
+        public override Task ExecuteAsync(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("至少需要一个参数或选项");
+                return Task.CompletedTask;
+            }
+            var option = Options.FirstOrDefault(x => x.Name == args[0]);
+            option?.Action.Invoke();
+            return Task.CompletedTask;
+        }
+    }
+}
