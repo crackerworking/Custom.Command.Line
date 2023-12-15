a6670725ab34184a57d1013eeefa9d219bad9e50
cracker <crackerwork@126.com>
2023-12-15 14:16:35 +0800

    励志短句命令

---
+++ Custom.CLI/Commands/ImproveCommand.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json.Linq;
+
+using Spectre.Console;
+using Spectre.Console.Cli;
+
+namespace Custom.CLI.Commands
+{
+    internal class ImproveCommand : Command
+    {
+        public override int Execute(CommandContext context)
+        {
+            var client = new HttpClient();
+            var msg = client.GetAsync("https://hmajax.itheima.net/api/ambition").GetAwaiter().GetResult();
+            var str = msg.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+            var jo = JObject.Parse(str);
+            AnsiConsole.Write(new Markup("[bold cyan1]" + jo["data"] + "！[/]"));
+            return 0;
+        }
+    }
+}
