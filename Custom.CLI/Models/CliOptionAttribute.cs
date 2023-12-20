42a4f7fc81dadb8f0102863ca3114bbcc18b2482
cracker <crackerwork@126.com>
2023-12-20 15:47:56 +0800

    style

--- Custom.CLI/Models/CliOptionAttribute.cs
+++ Custom.CLI/Models/CliOptionAttribute.cs
@@ -3,8 +3,8 @@
     [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
     public class CliOptionAttribute(string name, string description) : Attribute
     {
-        public string Name { get; private set; } = name;
+        public string Name => name;
 
-        public string Description { get; private set; } = description;
+        public string Description => description;
     }
 }
